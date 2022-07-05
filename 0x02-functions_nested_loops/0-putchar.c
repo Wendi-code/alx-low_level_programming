962893c6cd3144facb43acf59a3bd7146cd47154
wendi-code <wendimusit@gmail.com>
2022-07-05 21:19:58 +0300

    solve 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+ * File: 0-putchar.c
+ * Author: Wendimu S. Tarekegn
+ */
+
+/**
+ * main: Prints _putchar followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return 0;
+}
