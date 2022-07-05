5ebf87e31eee385d0ef85b82ea22cf364555b5bd
wendi-code <wendimusit@gmail.com>
2022-07-05 21:19:10 +0300

    write the first prototype in the header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/*
+ * File: main.h
+ * Auth: Wendimu S. Tarekegn
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+
+#endif
