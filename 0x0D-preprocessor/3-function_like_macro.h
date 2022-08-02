b37b9cabff5caf3c31fb321be026772aab0f33a8
Wendi-code <wenndimusit@gmail.com>
2022-08-02 17:13:02 +0300

    solve 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,8 +3,8 @@
 
 /*
  * File: 3-function_like_macro.h
- * Auth: Brennan D Baraban
- * Desc: Header file that defines a function-like macro ABS(x)
+ * Author: Wendimu S Tarekegn
+ * Description: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */
 
